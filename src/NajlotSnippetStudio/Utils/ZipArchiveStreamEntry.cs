36d74efd83de214bc71689a69c2560f7009f12a0
najlot <github.najlot@gmail.com>
2018-06-17 22:29:42 +0200

    Some utils

---
+++ src/NajlotSnippetStudio/Utils/ZipArchiveStreamEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace NajlotSnippetStudio.Utils
+{
+	public class ZipArchiveStreamEntry
+	{
+		public string EntryName { get; private set; }
+		public Stream Stream { get; private set; }
+
+		public ZipArchiveStreamEntry(string entryName, Stream stream)
+		{
+			EntryName = entryName;
+			Stream = stream;
+		}
+	}
+}
