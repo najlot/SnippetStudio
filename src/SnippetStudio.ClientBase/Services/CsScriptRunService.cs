063ba3c8340bd62a2942a8414548a2ab90d8873b
najlot <najlot@users.noreply.github.com>
2020-10-10 10:05:31 +0200

    fix

--- src/SnippetStudio.ClientBase/Services/CsScriptRunService.cs
+++ src/SnippetStudio.ClientBase/Services/CsScriptRunService.cs
@@ -3,62 +3,44 @@ using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CodeAnalysis.Scripting.Hosting;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SnippetStudio.ClientBase.Services
 {
 	public class CsScriptRunService
 	{
-		private InteractiveAssemblyLoader loader;
-		private ScriptOptions options;
+		private readonly InteractiveAssemblyLoader _loader;
+		private readonly ScriptOptions _options;
 		private readonly IClipboardService _clipboardService;
 
 		public CsScriptRunService(IClipboardService clipboardService)
 		{
 			_clipboardService = clipboardService;
-		}
 
-		private readonly System.Reflection.Assembly[] references = new[]
-		{
-			typeof(object).Assembly,
-			typeof(System.IO.FileInfo).Assembly,
-			typeof(System.Linq.IQueryable).Assembly,
-			typeof(System.Dynamic.DynamicObject).Assembly,
-			typeof(System.Text.RegularExpressions.Regex).Assembly
-		};
-
-		private InteractiveAssemblyLoader GetLoader()
-		{
-			if (loader == null)
+			var types = new[]
 			{
-				loader = new InteractiveAssemblyLoader();
+				typeof(object),
+				typeof(System.IO.FileInfo),
+				typeof(System.Linq.IQueryable),
+				typeof(System.Threading.Tasks.Task),
+				typeof(System.Dynamic.DynamicObject),
+				typeof(System.Collections.Generic.List<>),
+				typeof(System.Text.RegularExpressions.Regex),
+				typeof(System.Collections.Concurrent.ConcurrentDictionary<,>),
+				typeof(Newtonsoft.Json.JsonConvert),
+			};
 
-				foreach (var reference in references)
-				{
-					loader.RegisterDependency(reference);
-				}
+			var references = types.Select(type => type.Assembly).ToArray();
+
+			_loader = new InteractiveAssemblyLoader();
+			foreach (var reference in references)
+			{
+				_loader.RegisterDependency(reference);
 			}
 
-			return loader;
-		}
-
-		private ScriptOptions GetOptions()
-		{
-			if (options == null)
-			{
-				options = ScriptOptions.Default
-					.WithReferences(references)
-					.AddImports(
-						"System",
-						"System.IO",
-						"System.Linq",
-						"System.Text",
-						"System.Dynamic",
-						"System.Collections.Generic",
-						"System.Text.RegularExpressions"
-						);
-			}
-
-			return options;
+			_options = ScriptOptions.Default
+				.WithReferences(references)
+				.AddImports(types.Select(type => type.Namespace).ToArray());
 		}
 
 		public async Task<string> Run(string code, string template, Dictionary<string, string> variables)
@@ -70,12 +52,12 @@ namespace SnippetStudio.ClientBase.Services
 			};
 
 			var run = CSharpScript
-					.Create(code, GetOptions(), typeof(Globals), GetLoader())
+					.Create(code, _options, typeof(Globals), _loader)
 					.CreateDelegate();
 
 			var result = await run(globals);
 
-			return result.ToString();
+			return result?.ToString();
 		}
 	}
 }
